d3d1072cdf07b9c9074876fb2f32c127cbc36a82
Edward Eldridge <steadyeddie101@hotmail.co.uk>
2018-04-30 16:05:59 +0000

    Added reading in foobar2000 library json export

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import urllib.parse
 import requests
+import json
+from pprint import pprint
 
 # Set the keys required to access the Discogs api
 consumer_key = "uTweskQdRQLFmQcACKVo"
@@ -8,17 +10,25 @@ secret_key = "qyprAKEvdbRJAuFzPPdRbyELDPcYgyQO"
 # Define the discogs api
 main_api = 'https://api.discogs.com/database/search?q=SEARCHTERM&key=CONSUMERKEY&secret=SECRETKEY'
 
+#Read in our file containing information about our Foobar library
+data = json.load(open('test.json'))
+
+data["meta"]["ALBUM"]
+
+# Pretty print our data
+pprint(data)
+
 # User Input for their search term
-searchTerm = input("Search: " )
+#searchTerm = input("Search: " )
 
 # Construct our URL with keys and search term
-url = main_api.replace("SEARCHTERM", searchTerm)
-url = url.replace("CONSUMERKEY", consumer_key)
-url = url.replace("SECRETKEY", secret_key)
+#url = main_api.replace("SEARCHTERM", searchTerm)
+#url = url.replace("CONSUMERKEY", consumer_key)
+#url = url.replace("SECRETKEY", secret_key)
 
 # Print the request url
-print ('Requesting: ', url)
+#print ('Requesting: ', url)
 
-#
-json_data = requests.get(url).json()
-print(json_data)
+# Print our requested data
+#requested_data = requests.get(url).json()
+#print(requested_data)
